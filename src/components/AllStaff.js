bf0595f2384131c61ae600bcd2f07b4dbcff8472
Gianelle Tianco <gianelle.tianco@gmail.com>
2022-01-28 01:01:01 -0500

    Added react-modal

--- src/components/AllStaff.js
+++ src/components/AllStaff.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-import Modal from './Modal'
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
 
 function AllStaff (props) {
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+    
     const loaded = () =>{
         return props.Employee.map((staff) => (
+            
             <tr key={staff.id}>
                <th scope='row'>{staff.firstName} {staff.lastName}</th>
                <td>{staff.jobTitle}</td>
                <td>{staff.department}</td>
                <td>
-                   <Link to={`/`}><i className='bi bi-person-dash-fill'id="delete" onClick={() => props.deleteEmployee(staff.id)}></i></Link>
-                   <Modal updateEmployee={updateEmployee}/>
+                    <i className='bi bi-eyeglasses' onClick={() => setModalIsOpen(true)}></i>&nbsp; &nbsp; &nbsp;
+                    <Modal 
+                    isOpen={modalIsOpen} 
+                    onRequestClose={() => setModalIsOpen(false)}
+                    style={{
+                        overlay: {
+                            backgroundColor: 'gray'
+                        },
+                        content: {
+                            color: '#097392', 
+                            left: '120px',
+                            right: '120px',
+                        }
+                    }}>
+                        <h3>{staff.lastName}, {staff.firstName}</h3>
+                        <div>
+                        <i className='bi bi-x-circle' onClick={() => setModalIsOpen(false)}></i>
+                        </div>
+                    </Modal>
+                    <i className='bi bi-pencil-square'></i>&nbsp; &nbsp; &nbsp;
+                   <Link to={`/`}>
+                    <i className='bi bi-person-dash-fill'id="delete" onClick={() => props.deleteEmployee(staff.id)}></i>
+                    </Link>
                 </td>
             </tr>
         ))
@@ -27,7 +53,7 @@ function AllStaff (props) {
                         <th scope='col'>Employee Name</th>
                         <th scope='col'>Job Title</th>
                         <th scope='col'>Department</th>
-                        <th scope='col'>Action Item</th>
+                        <th scope='col'>Action Items</th>
                     </tr>
                 </thead>
                 <tbody>
